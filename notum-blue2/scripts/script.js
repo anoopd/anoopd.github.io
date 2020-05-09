4b7ef7c105d6caf2f063cab325e7224d507018e6
Anoop D <anoopd1@hotmail.com>
2020-05-09 19:54:57 +0530

    first Tweaked

--- notum-blue2/scripts/script.js
+++ notum-blue2/scripts/script.js
@@ -19,11 +19,11 @@ var tl  = gsap.timeline()
 tl.from('.logo img',{ opacity:0, ease: 'power1.inOut' })
 .from('.logo h1',{ opacity:0, ease: 'power1.inOut' })
 .from('.logo span',{ opacity:0, ease: 'power1.inOut' })
-.from('.menu li',{opacity: 0,x:20,stagger:0.1,ease:"power1.inOut"})
+.from('.menu li',{opacity: 0,x:20,stagger:0.1,ease:"power1.inOut"},"-=1")
 .from('.teaser-container__image img',{opacity: 0, y:40, ease:'power1.inOut'})
-.from('.teaser-container__content h1',{ opacity:0, ease: 'power1.inOut' })
+.from('.teaser-container__content h1',{ opacity:0, ease: 'power1.inOut' },"-=1")
 .from('.down1',{opacity: 0, x:-40, ease:'power1.inOut'})
-.from('.down2',{opacity: 0, x:40, ease:'power1.inOut'})
+.from('.down2',{opacity: 0, x:40, ease:'power1.inOut'},"<")
 
 var controller = new ScrollMagic.Controller();
 
@@ -31,9 +31,13 @@ var controller = new ScrollMagic.Controller();
 
 var action = gsap.timeline()
 
-action.from('.features-wrapper__one',{opacity:0,y:-20, ease:'power1.inOut'})
+action.from('.features-wrapper__one .svg',{opacity:0,y:-20, ease:'power1.inOut'})
+.from('.features-wrapper__one .cont',{opacity:0,y:10,ease:'poser1.inOut'})
 .from('.features-wrapper__two',{opacity:0,y:-20, ease:'power1.inOut'})
+.from('.features-wrapper__two .cont',{opacity:0,y:10,ease:'poser1.inOut'})
 .from('.features-wrapper__three',{opacity:0,y:-20, ease:'power1.inOut'})
+.from('.features-wrapper__three.cont',{opacity:0,y:10,ease:'poser1.inOut'})
+
 
 var scene = new ScrollMagic.Scene({
     triggerElement: '.features-wrapper__one',
@@ -46,10 +50,9 @@ var action1 = gsap.timeline()
 
 action1.from('.features-wrapper h3',{opacity:0,y:-20,ease:'power1.inOut'})
 .from('.list_one h4',{opacity:0, x:-20, ease: 'power1.inOut'})
-.from('.list_two h4',{opacity:0, x:20, ease: 'power1.inOut'})
 .from('.list_one ul li',{opacity:0, y:20, stagger:0.1})
 .from('.list_two ul li',{opacity:0, y:20, stagger:0.1})
-.from('.features-wrapper img',{ opacity: 0, y:'100%',ease:'Expo.easeInOut' })
+.from('.features-wrapper img',{ opacity: 0, y:'50%',ease:'Expo.easeInOut' },"-=1")
      
 var scene1 = new ScrollMagic.Scene({
     triggerElement: '.features-wrapper h3',
